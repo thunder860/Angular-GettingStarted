79f29bd6167255dc5ad2b8df69a10ddb08abea92
Wisniewski <bobbywisniewski@amrock.com>
2019-01-23 13:42:27 -0500

    End of Module Templates, Interpolation, and directives

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  template: `
-  <div><h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
-  </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ProductListComponent ],
   imports: [ BrowserModule ],
   bootstrap: [ AppComponent ]
 })
