01d8d10f7b04a370b2a63ed7dbfca8cb02dcc826
Wisniewski <bobbywisniewski@amrock.com>
2019-01-25 08:15:43 -0500

    end module "Services and Dependency Injection"

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -22,32 +23,9 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [  
-        {
-            productId: 1,
-            productName: "Leaf Rake",
-            productCode: "GDN-0011",
-            releaseDate: "March 19, 2016",
-            description: "Leaf rake with 48-inch wooden handle.",
-            price: 19.95,
-            starRating: 3.2,
-            imageUrl: "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            productId: 2,
-            productName: "Garden Cart",
-            productCode: "GDN-0023",
-            releaseDate: "March 18, 2016",
-            description: "15 gallon capacity rolling garden cart",
-            price: 32.99,
-            starRating: 4.2,
-            imageUrl: "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
+    products: IProduct[];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    constructor(private productService: ProductService) {
     }
 
     performFilter(filterBy: string): IProduct[]{
@@ -61,7 +39,8 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log('In OnInit');
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 
     onRatingClicked(message: string): void {
