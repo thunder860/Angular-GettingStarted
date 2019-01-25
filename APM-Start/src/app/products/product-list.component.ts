7f858a185c54ca98e91540089282e28b6eaee6d7
Wisniewski <bobbywisniewski@amrock.com>
2019-01-25 07:39:17 -0500

    end module more on components

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,39 +1,66 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFiler: string = 'cart';
-    products: any[] = [  
+
+    _listFiler: string;
+    get listFilter(): string {
+        return this._listFiler;
+    }
+    set listFilter(value:string){
+        this._listFiler = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [  
         {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+            productId: 1,
+            productName: "Leaf Rake",
+            productCode: "GDN-0011",
+            releaseDate: "March 19, 2016",
+            description: "Leaf rake with 48-inch wooden handle.",
+            price: 19.95,
+            starRating: 3.2,
+            imageUrl: "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
         },
         {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+            productId: 2,
+            productName: "Garden Cart",
+            productCode: "GDN-0023",
+            releaseDate: "March 18, 2016",
+            description: "15 gallon capacity rolling garden cart",
+            price: 32.99,
+            starRating: 4.2,
+            imageUrl: "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         }
     ];
 
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+
+    performFilter(filterBy: string): IProduct[]{
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
     }
 }
---
+++ APM-Start/src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'convertToSpaces'
+})
+export class ConvertToSpacesPipe implements PipeTransform {
+    transform(value: any, character: string) {
+        return value.replace(character, ' ');
+    }
+}
