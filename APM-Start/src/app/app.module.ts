fbe83dabfb8394ce479f35d6f041752121cacd32
Wisniewski <bobbywisniewski@amrock.com>
2019-01-25 09:12:50 -0500

    end module "Retreiving Data using http"

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -14,7 +15,11 @@ import { StarComponent } from './shared/star.component';
     ConvertToSpacesPipe,
     StarComponent
   ],
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ 
+    BrowserModule, 
+    FormsModule,
+    HttpClientModule
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -12,8 +12,9 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-
     _listFiler: string;
+    errorMessage: string;
+
     get listFilter(): string {
         return this._listFiler;
     }
@@ -39,8 +40,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.productService.getProducts().subscribe(
+            products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error
+        );
     }
 
     onRatingClicked(message: string): void {
