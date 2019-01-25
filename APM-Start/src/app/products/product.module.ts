8e248b6a61f7609cbb9947dd0354160444f5ef6d
Wisniewski <bobbywisniewski@amrock.com>
2019-01-25 11:20:24 -0500

    end module "Angular Modules"

---
+++ APM-Start/src/app/products/product.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { SharedModule } from '../shared/shared.module';
+import { ProductRoutingModule } from './product-routing.module';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    ProductRoutingModule
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent
+  ],
+})
+export class ProductModule { }
